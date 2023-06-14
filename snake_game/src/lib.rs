ac8add517e04d3e05f7fd4cd3390c8e297ca5f45
Giwoo Lee <gglee89@gmail.com>
2023-06-14 07:55:09 +0700

    integrate wasm

--- snake_game/src/lib.rs
+++ snake_game/src/lib.rs
@@ -1,9 +1,18 @@
-
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-  println!("Hi there {}", name);
+  alert(name)
+}
+
+#[wasm_bindgen]
+extern {
+  pub fn alert(s: &str);
 }
 
 // wasm-pack build --target web ---> targetting JS
--- snake_game/www/index.js
+++ snake_game/www/index.js
@@ -1,31 +1,5 @@
-async function init() {
-  const memory = new WebAssembly.Memory({ initial: 1 }); // 1 page, 64KB
+import init, { greet } from "snake_game";
 
-  const importObject = {
-    js: {
-      mem: memory,
-    },
-    console: {
-      log: () => {
-        console.log("Just logging something");
-      },
-      error: () => {
-        console.log("I am just error");
-      },
-    },
-  };
-
-  // Fetch out WASM file
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-
-  debugger;
-  await WebAssembly.instantiate(buffer, importObject);
-  debugger;
-  const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-
-  const hiText = new TextDecoder().decode(uint8Array); // Decode from bytes
-  console.log("hiText", hiText);
-}
-
-init();
+init().then(() => {
+  greet("Philip 안녕");
+});
